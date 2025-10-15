addbfbdd0978c7e2009a02e6b6b9a74282400650
zararafridi <132477878+zararafridi@users.noreply.github.com>
2025-10-16 01:16:39 +0500

    Update footer copyright text from coinBounce to Blocksy

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import style from '../footer/Footer.module.css'
 function Footer() {
   return (
     <div>
-      <p className={style.footer}> &copy; coinBounce 2023</p>
+      <p className={style.footer}> &copy; Blocksy 2023</p>
     </div>
   )
 }
